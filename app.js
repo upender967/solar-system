65c8ffc899bf4f4898f82510f3f7d01d898bd48b
jeep <Jeep9492@gmail.com>
2025-03-14 23:16:47 -0400

    update depdency

--- app.js
+++ app.js
@@ -13,9 +13,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI, {
-    user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
+mongoose.connect('mongodb+srv://superuser:SuperPassword@supercluster.d83jj.mongodb.net/superData', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
