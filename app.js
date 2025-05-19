6c3398c26c88862c199d13ff2008c6fe54e12efe
zim0101 <cybertronix.4406@gmail.com>
2025-05-19 17:10:23 +0600

    update app.js with .env variables

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
 const path = require('path');
-const fs = require('fs')
+const fs = require('fs');
 const express = require('express');
 const OS = require('os');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const app = express();
-const cors = require('cors')
-const serverless = require('serverless-http')
+const cors = require('cors');
+const serverless = require('serverless-http');
 
+// Load environment variables from .env file
+require('dotenv').config();
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
-app.use(cors())
+app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, {
-    user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
+const mongoUri = process.env.MONGO_URI;
+const mongoUsername = process.env.MONGO_USERNAME;
+const mongoPassword = process.env.MONGO_PASSWORD;
+const nodeEnv = process.env.NODE_ENV;
+const appPort = process.env.APP_PORT || 3000;
+
+console.log(`Connecting to MongoDB at: ${mongoUri}`);
+
+mongoose.connect(mongoUri, {
+    user: mongoUsername,
+    pass: mongoPassword,
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
     if (err) {
-        console.log("error!! " + err)
+        console.log("MongoDB connection error: " + err);
     } else {
-      //  console.log("MongoDB Connection Successful")
+        console.log("MongoDB Connection Successful");
     }
-})
+});
 
 var Schema = mongoose.Schema;
 
@@ -38,60 +48,61 @@ var dataSchema = new Schema({
 });
 var planetModel = mongoose.model('planets', dataSchema);
 
-
-
-app.post('/planet',   function(req, res) {
-   // console.log("Received Planet ID " + req.body.id)
+app.post('/planet', function(req, res) {
+    console.log("Received Planet ID " + req.body.id);
     planetModel.findOne({
         id: req.body.id
     }, function(err, planetData) {
         if (err) {
-            alert("Ooops, We only have 9 planets and a sun. Select a number from 0 - 9")
-            res.send("Error in Planet Data")
+            console.error("Error retrieving planet data:", err);
+            res.status(500).send("Error in Planet Data");
         } else {
             res.send(planetData);
         }
-    })
-})
+    });
+});
 
-app.get('/',   async (req, res) => {
+app.get('/', async (req, res) => {
     res.sendFile(path.join(__dirname, '/', 'index.html'));
 });
 
 app.get('/api-docs', (req, res) => {
     fs.readFile('oas.json', 'utf8', (err, data) => {
-      if (err) {
-        console.error('Error reading file:', err);
-        res.status(500).send('Error reading file');
-      } else {
-        res.json(JSON.parse(data));
-      }
+        if (err) {
+            console.error('Error reading file:', err);
+            res.status(500).send('Error reading file');
+        } else {
+            res.json(JSON.parse(data));
+        }
     });
-  });
-  
-app.get('/os',   function(req, res) {
+});
+
+app.get('/os', function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send({
         "os": OS.hostname(),
-        "env": process.env.NODE_ENV
+        "env": nodeEnv
     });
-})
+});
 
-app.get('/live',   function(req, res) {
+app.get('/live', function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send({
         "status": "live"
     });
-})
+});
 
-app.get('/ready',   function(req, res) {
+app.get('/ready', function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send({
         "status": "ready"
     });
-})
+});
 
-app.listen(3001, () => { console.log("Server successfully running on port - " +3001); })
+app.listen(appPort, () => {
+    console.log("Server successfully running on port - " + appPort);
+});
+
 module.exports = app;
 
-//module.exports.handler = serverless(app)
+//module.exports.handler = serverless(app);
